a77e4124c2a38622cdb8ec6ce4b8891eb4af0f34
Pawel <ppazyrski@wp.pl>
2025-05-12 21:09:04 +0200

    Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://first-astroo.netlify.app/"
+  site: "https://first-astroo.netlify.app/",
+  integrations: [preact()]
 });
